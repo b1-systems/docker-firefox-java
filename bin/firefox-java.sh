84ac059dd496cfc4f97b10298c8aa3d7f4208592
Uwe Grawert <grawert@b1-systems.de>
2019-07-25 10:37:30 +0200

    Add start script for host command line

---
+++ bin/firefox-java.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Put me into your PATH
+
+IMAGE="${IMAGE:-ugrawert/firefox-java}"
+X11_SOCKET="${X11_SOCKET:-/tmp/.X11-unix}"
+DOCKER_DISPLAY="${DOCKER_DISPLAY:-unix${DISPLAY}}"
+DOWNLOADS="${DOWNLOADS:-${HOME}/Downloads}"
+uid=${uid:-$(id -u)}
+gid=${gid:-$(id -g)}
+URL=${URL:-$1}
+
+xhost | grep -q 'LOCAL' || echo "You might have to run 'xhost +local:docker'"
+
+docker run --rm -ti \
+           -v "${DOWNLOADS}:/home/firefox/Downloads:rw" \
+           -v "${X11_SOCKET}:/tmp/.X11-unix" \
+           -e "DISPLAY=${DOCKER_DISPLAY}" \
+           -e "uid=${uid}" \
+           -e "gid=${gid}" \
+           -e "URL=${URL}" \
+           ${IMAGE}
